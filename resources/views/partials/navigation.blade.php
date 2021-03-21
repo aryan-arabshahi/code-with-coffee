e810213dd980251814c12766c168567bc36cdd82
Aryan Arabshahi <aryan.arabshahi.programmer@gmail.com>
2021-03-21 23:41:29 +0330

    Fixing the iPad responsive issue

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -6,7 +6,7 @@
             <li><a href="{{route('home.about')}}">About</a></li>
             <li><a href="{{route('home.contact')}}">Contact</a></li>
         </ul>
-        <a style="top: 8px;" class="absolute left-0" href="/" title="{{config('app.name')}}">
+        <a id="navigation-logo" class="absolute left-0" href="/" title="{{config('app.name')}}">
             <img class="block w-10" src="/assets/images/code-with-coffee.png" alt="{{config('app.name')}}">
         </a>
     </div>
